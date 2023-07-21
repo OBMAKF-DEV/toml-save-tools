22bd1eaaf68dca553a0659485ba0353574c79080
OBMAKF-DEV <140197512+OBMAKF-DEV@users.noreply.github.com>
2023-07-22 02:12:03 +0800

    Update __init__.py
    License and repo address updated.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
-# /=============================================\
-# |  File-Path  | ../savefile/__init__.py       |
-# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# |    Package  | savefile                      |
-# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# |             | - decorators.load_game        |
-# |    Modules  | - decorators.save_game        |
-# |             | - utils.load                  |
-# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# |     Github  | https://github.com/OBMAKF-DEV |
-# \=============================================/
+# /=============================================================\
+# |  File-Path  | ../toml-save-tools/__init__.py                |
+# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# |    Package  |   toml-save-tools                             |
+# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# |             | - decorators.load_game                        |
+# |    Modules  | - decorators.save_game                        |
+# |             | - utils.load                                  |
+# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# |     Github  | https://github.com/OBMAKF-DEV/toml-save-tools |
+# |~~~~~~~~~~~~~+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# |    License  |  MIT License Copyright (c) 2023 OBMAKF-DEV    |
+# \=============================================================/
 
 """This Package provides save file functionality for saving and loading from a TOML save file."""
 
